af888b46bcd54bded1fedb17cf5ad62d2709387b
Phil Fevry <phil@auroradigit.al>
2020-03-08 20:14:15 -0700

    declare all view controllers

---
+++ Restaraunt/MenuItemDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuItemDetailViewController.swift
+//  Restaraunt
+//
+//  Created by Phil on 3/8/20.
+//  Copyright © 2020 AURORA Digital. All rights reserved.
+//
+
+import UIKit
+
+class MenuItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
