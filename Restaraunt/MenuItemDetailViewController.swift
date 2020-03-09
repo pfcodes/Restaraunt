1b397fef65eb4e6051768bbc973454258271b097
Phil Fevry <phil@auroradigit.al>
2020-03-08 22:30:10 -0700

    began implementing detail storyboard interface + view controller implementations

--- Restaraunt/MenuItemDetailViewController.swift
+++ Restaraunt/MenuItemDetailViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var detailTextLabel: UILabel!
+  @IBOutlet weak var addToOrderButton: UIButton!
+  
   var menuItem: MenuItem!
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     title = menuItem.name
 
-    // Do any additional setup after loading the view.
+    addToOrderButton.layer.cornerRadius = 5.0
+    updateUI()
+  }
+  
+  func updateUI() {
+    titleLabel.text = menuItem.name
+    priceLabel.text = String(format: "$%.2f", menuItem.price)
+    detailTextLabel.text = menuItem.detailText
   }
     
 
