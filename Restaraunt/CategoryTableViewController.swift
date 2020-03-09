0d78a94f0015a9132ecc5a7f7f165a5eae089e49
Phil Fevry <phil@auroradigit.al>
2020-03-08 21:23:44 -0700

    implement data fetching and rendering

--- Restaraunt/CategoryTableViewController.swift
+++ Restaraunt/CategoryTableViewController.swift
@@ -9,82 +9,93 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
+  let menuController = MenuController()
+  var categories = [String]()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    menuController.fetchCategories { (categories) in
+      if let categories = categories {
+        self.categories = categories
+        self.updateUI(with: categories)
+      }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+  }
+  
+  func updateUI(with categories: [String]) {
+    DispatchQueue.main.async {
+      self.categories = categories
+      self.tableView.reloadData()
     }
+  }
+  
+  func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+    cell.textLabel?.text = categories[indexPath.row].capitalized
+  }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+  // MARK: - Table view data source
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
 
-        // Configure the cell...
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return categories.count
+  }
 
-        return cell
-    }
-    */
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCellIdentifier", for: indexPath)
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+    // Configure the cell...
+    configure(cell, forItemAt: indexPath)
+    
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    return cell
+  }
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+  /*
+  // Override to support conditional editing of the table view.
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+      // Return false if you do not want the specified item to be editable.
+      return true
+  }
+  */
 
-    }
-    */
+  /*
+  // Override to support editing the table view.
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+      if editingStyle == .delete {
+          // Delete the row from the data source
+          tableView.deleteRows(at: [indexPath], with: .fade)
+      } else if editingStyle == .insert {
+          // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+      }
+  }
+  */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+  /*
+  // Override to support rearranging the table view.
+  override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
-    /*
-    // MARK: - Navigation
+  }
+  */
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  /*
+  // Override to support conditional rearranging of the table view.
+  override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+      // Return false if you do not want the item to be re-orderable.
+      return true
+  }
+  */
+
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      // Get the new view controller using segue.destination.
+      // Pass the selected object to the new view controller.
+  }
+  */
 
 }
