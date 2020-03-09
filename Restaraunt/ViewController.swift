9bf6e55e4bd8b1dd8cf7984c6bfaffd2bb9447f3
Phil Fevry <phil@auroradigit.al>
2020-03-08 19:58:06 -0700

    Initial Commit

---
+++ Restaraunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaraunt
+//
+//  Created by Phil on 3/8/20.
+//  Copyright © 2020 AURORA Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
