9f37d60d8d9d0b4d5f62622ec7b31f99a4172279
Phil Fevry <phil@auroradigit.al>
2020-03-09 16:06:11 -0700

    implement cache

--- Restaraunt/AppDelegate.swift
+++ Restaraunt/AppDelegate.swift
@@ -13,6 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    let temporaryDirectory = NSTemporaryDirectory()
+    let urlCache = URLCache(
+      memoryCapacity: 25_000_000,
+      diskCapacity: 50_000_000,
+      diskPath: temporaryDirectory
+    )
+    URLCache.shared = urlCache
+
     return true
   }
 
